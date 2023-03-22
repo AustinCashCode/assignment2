6e2c034cc95e8d73ee02113c48c4fc3e7ff1ab05
Austin Cash <cashau@oregonstate.edu>
2023-03-21 23:36:16 -0700

    Finished! Yay!

--- main.c
+++ main.c
@@ -5,6 +5,8 @@
 #include <sys/stat.h>
 #include <limits.h>
 #include <time.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 #define IO_BUFFER 256
 
@@ -15,25 +17,111 @@ typedef struct my_movie {
     char * language;
     double rating;
 
-    struct my_movie * left;
+    struct my_movie * next;
 
 } movie_t;
 
 
-void process_file(FILE *file, char * filename) {
-    movie_t * movies[1024];
+//Taken from my assignment 1 code
+movie_t * append_movie(movie_t * head, movie_t * new_movie)
+{
+    new_movie -> next = head;
+    head = new_movie;
+    return head;
+}
 
-    printf("Now processing the chosen file named %s", filename);
 
-    //Making the directory
+//Also from assignment 1
+movie_t * extract_movie(char * raw_data)
+{
+	// Data in the loaded file is assumed to 
+	// have the following structure:
+	// Title,Year,Languages,Rating Value
+	// EX: The Incredible Hulk,2008,[English;Portuguese;Spanish],6.8
+    movie_t * to_add = malloc(sizeof(movie_t));
+    char * saveptr;
+    char * token;
+    
+	// First is the name
+    token = strtok_r(raw_data, ",", &saveptr);
+    to_add -> title = malloc(strlen(token) + 2);
+    strcpy(to_add -> title, token);
+
+
+    // Next is the release year as an integer
+    token = strtok_r(NULL, ",", &saveptr);
+    to_add -> year = atoi(token);
+    
+    // Next is the string of languages.
+    token = strtok_r(NULL, ",", &saveptr);
+    to_add -> language = malloc(strlen(token) + 1);
+    strcpy(to_add -> language, token);
+
+    // Finally the rating, stored as a double
+    token = strtok_r(NULL, ",", &saveptr);
+    to_add -> rating = strtod(token, NULL);
+
+    to_add -> next = NULL;
+
+    return to_add;
+}
+
+
+
+//Once again from my assignment 1.
+movie_t * load_file (FILE * to_load, movie_t * head) 
+{
+    int count = 0;
+    char * line = NULL;
+    size_t length = 0;
+    ssize_t nread;
+    movie_t * temp;
+
+    //Since the first line of the file is garbage,
+    //we just throw it away here.
+    nread = getline(&line, &length, to_load);
+
+    while((nread = getline(&line, &length, to_load) != -1))
+    {
+        temp = extract_movie(line);
+        head = append_movie(head, temp);
+        count++;
+    }
+    free(line);
+    fclose(to_load);
+    return head;
+}
+
+
+
+void process_file(FILE *file, char *filename) {
+    movie_t *head;
+    movie_t *current;
+
+    printf("Now processing the chosen file named %s\n", filename);
+
+    // Making the directory
     char dirname[256];
     srand(time(NULL));
     int random = rand() % 10000;
-    sprintf(dirname, "./cashau_%d.movies", random);
+    snprintf(dirname, sizeof(dirname), "./cashau_%d.movies", random);
     mkdir(dirname, S_IRWXU | S_IRGRP | S_IXGRP);
 
-    
+    // Getting the input
+    head = load_file(file, head);
+    current = head;
 
+
+    while (current) {
+        char file_path[256];
+        snprintf(file_path, sizeof(file_path), "%s/%d.txt", dirname, current->year);
+        int fd = open(file_path, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR | S_IRGRP);
+        write(fd, current->title, strlen(current->title));
+        write(fd, "\n", 1);
+        close(fd);
+
+        current = current -> next;
+    }
 
     return;
 }
@@ -70,10 +158,10 @@ void process_largest() {
 
         if(!file) {
             fprintf(stderr, "Could not open largest file\n");
-        }
+        }   
+        printf("FILE: %s\n\n", largest_fname);
 
         process_file(file, largest_fname);
-        fclose(file);
     }
     else {
         fprintf(stderr, "No file matching format: movies_*.csv\n");
@@ -118,7 +206,6 @@ void process_smallest() {
         }
 
         process_file(file, smallest_fname);
-        fclose(file);
     }
     else {
         fprintf(stderr, "No file matching format: movies_*.csv\n");
@@ -164,7 +251,6 @@ int process_custom() {
     }
     
     process_file(file, buffer);
-    fclose(file);
 
     return 1;
 }
